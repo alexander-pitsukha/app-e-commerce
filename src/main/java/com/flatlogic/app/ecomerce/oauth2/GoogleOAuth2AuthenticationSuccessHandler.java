6b32734aee4399aff6201ce949b9ee2b11a31374
Alexander Pitsukha <alexanderp@ligolab.com>
2024-02-12 01:09:19 +0300

    dev

--- src/main/java/com/flatlogic/app/ecomerce/oauth2/GoogleOAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/flatlogic/app/ecomerce/oauth2/GoogleOAuth2AuthenticationSuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
